d7b20c3737e96bdbf60e2f200d5b94e23fcca1f6
P-Nasty <31251496+P-Nasty@users.noreply.github.com>
2017-10-09 09:58:08 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,22 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/response")
+def render_response():
+    color = request.args['color']
+    #stores information sent to the server
+    #args is a dactionary able to carry multiple values
+    #args is visible in the url of the website
+    if color == 'pink':
+        reply = "You must be a girl!!"
+    else:
+        reply = "you are a chump"
+    return render_template('response.html', response = reply)
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
